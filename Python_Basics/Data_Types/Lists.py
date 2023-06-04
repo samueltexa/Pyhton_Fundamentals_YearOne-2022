2d8fe9956ae1884be6d176900d82901cbec8da77
samueltexa <woolardsamuels@gmail.com>
2023-06-04 17:03:54 +0300

    master python list

--- Python_Basics/Data_Types/Lists.py
+++ Python_Basics/Data_Types/Lists.py
@@ -22,3 +22,59 @@ print(c[1]) #this will print "you" in list c
 
 print(a,b)
 print(c[0], b[1])
+
+"""Working with Lists and different operations performed on lists"""
+# Ways to modify a pyton list.
+""" Append method, Remove method, Pop method, Insert method, Sort method, Index method, Count method,
+Extend method"""
+
+"""Example"""
+# Defining list
+list = ["jack", "emma", "Dani", "Samuel", "Karen", "Karen"]
+print(list)
+list[0] = "Eddy" # Update the item at index 0 with the string "Eddy"
+
+# 1.Append method: Adds an element at the end of the list, therefore takes no arguments.
+list.append("Samuel")
+print(list)
+list.append("Alinda")
+print(list)
+
+# 2.Remove: Removes the given element from the list, it takes exactly one argument.
+list.remove("emma")
+print(list)
+
+# 3.Pop: Removes the given element at an index provided, it takes exactly one argument(index).
+list.pop(0)
+print(list)
+
+# 4.Insert: Add elements to the list at the index provided, it takes exactly two argument(index and the element).
+list.insert(0, "Karen")
+list.insert(1, "Jordan")
+list.insert(2, "Roy")
+print(list)
+
+# 5.Sort: Removes the given element from the list, it takes exactly one argument.
+list.sort() # from low to high, asscending to descending.
+print(list)
+list.sort(reverse=True) # from high to low, descending to asscending.
+print(list)
+
+# 6.Index: returens the index of a given element in the list.
+print(list.index("Samuel"))
+print(list.index("Karen"))
+print(list.index("Alinda"))
+
+# 7.Count: counts the number of times a value appears in a list.
+print(list.count("Karen"))
+print(list.count("Samuel"))
+
+# 8.Extend: This method extends a list by appending items. The benefit of this is you can add lists together.
+list2 =["sam", "henry", "linus"]
+list2.extend(list)
+print(list2)
+
+# Slicing a list
+print(list2[:3]) # prints elements of the list up to index 3-1
+print(list2[::3]) # prints the values of the list every after 2 elements(3-1).
+print(list2[::2]) # prints the values of the list every after 1 element(2-1)
