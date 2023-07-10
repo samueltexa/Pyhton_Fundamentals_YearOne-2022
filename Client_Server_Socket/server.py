fa20aa62ef77d9b6e2f11902a5073c3839828087
samueltexa <woolardsamuels@gmail.com>
2023-07-11 00:05:03 +0300

    Server side

--- Client_Server_Socket/server.py
+++ Client_Server_Socket/server.py
@@ -1,67 +1,69 @@
 import socket
-import tkinter as mygui
 from threading import Thread
 
-server_window = mygui.Tk()
-server_window.title("Server_Side")
-server_window.geometry("500x350")
-recievedMessagelbl = mygui.Label(server_window, text="Received messages:")
-recievedMessagelbl.grid(row=0,column=0)
-text_area = mygui.Text(server_window, state=mygui.DISABLED, height=10, width=60)
-text_area.grid(row=1, column=0)
-enterMessagelbl = mygui.Label(server_window , text="Enter your message Here:")
-enterMessagelbl.grid(row=2,column=0)
-text_field = mygui.Entry(server_window)
-text_field.grid()
+clients = []  # List to store connected clients
 
-
-def receive_messages(conn, text_area):
+def receive_messages(conn, address):
     while True:
-        data = conn.recv(1024).decode()
-        if not data:
+        try:
+            message = conn.recv(1024).decode()  # Receive message from client
+            if not message:
+                break
+            print("Received message from client", address, ":", message)
+            broadcast_message(message, conn)  # Broadcast the message to all other clients
+        except ConnectionResetError:
             break
-        text_area.config(state=mygui.NORMAL)
-        text_area.insert(mygui.END, data + '\n')
-        text_area.config(state=mygui.DISABLED)
+
+    disconnect_client(conn)  # Disconnect the client
+
+def disconnect_client(conn):
+    for client in clients:
+        if client[0] == conn:
+            address = client[1]
+            clients.remove(client)  # Remove the client from the list
+            print(f"\nClient {address} disconnected from server")
+            break
+
+def broadcast_message(message, sender_conn):
+    for client in clients:
+        conn, _ = client
+        if conn != sender_conn:
+            try:
+                conn.send(message.encode())  # Send the message to each connected client
+            except BrokenPipeError:
+                continue
+
+def client_thread(conn, address):
+    print("\nConnection from", address, "established successfully.")
+    clients.append((conn, address))  # Add the client to the list of connected clients
+
+    receive_thread = Thread(target=receive_messages, args=(conn, address))
+    receive_thread.daemon = True
+    receive_thread.start()
 
 def server_program():
-    def send_message():
-        message = text_field.get()
-        text_field.delete(0, mygui.END)
-        conn.send(message.encode())
-
-    def close_connection():
-        conn.close()
-        server_socket.close()
-        server_window.quit()
-        print("\nConnection from " + str(address) + " terminated successfully.")
-
     host = '0.0.0.0'
     port = 50000
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
-    server_socket.listen(2)
-    server_socket.settimeout(10)
+    server_socket.listen(5)  # Listen for incoming connections
+    print("\nServer started, waiting for connections.")
+    server_socket.settimeout(20)  # Set a timeout for accepting connections
 
     try:
-        conn, address = server_socket.accept()
-        print("\nConnection from " + str(address) + " accepted successfully.")
-
-        send_button = mygui.Button(server_window, text="Send", command=send_message, bg="green")
-        send_button.grid()
-        close_button = mygui.Button(server_window, text="Close", command=close_connection, bg="red")
-        close_button.grid()
-             
-        receive_thread = Thread(target=receive_messages, args=(conn, text_area))
-        receive_thread.daemon = True
-        receive_thread.start()
-
-        server_window.mainloop()
+        while True:
+            try:
+                conn, address = server_socket.accept()  # Accept a new client connection
+                client_thread(conn, address)  # Start a new thread to handle the client
+            except socket.timeout:
+                continue
     except Exception as e:
         print("An error occurred:", str(e))
     finally:
-        server_socket.close()
-        
+        for client in clients:
+            client[0].close()  # Close all client connections
+        server_socket.close()  # Close the server socket
 
-server_program()
+if __name__ == '__main__':
+    server_program()
