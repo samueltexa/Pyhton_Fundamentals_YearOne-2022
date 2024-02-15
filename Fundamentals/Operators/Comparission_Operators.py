d1a4d1c3ff06839992a52d92fb78002622b94723
samueltexa <woolarsamuels@gmail.com>
2024-02-15 10:01:38 +0300

    Comparission operators

---
+++ Fundamentals/Operators/Comparission_Operators.py
@@ -0,0 +1,16 @@
+# Comparison operators are used to compare two values:
+x = 10
+y = 10
+z = 11
+
+print(x == y)
+print(x == z)
+
+print(x != y)
+
+print(x > y)
+print(x < z)
+
+print(x >= y)
+print(x >= z)
+print(x <= z)
