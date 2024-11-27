a698f4c3ff91e27921b0722cb8e74983c2e1d07e
samueltexa <woolardsamuels@gmail.com>
2024-11-27 12:54:42 +0300

    add platform#fech device name

---
+++ client_server/platforms.py
@@ -0,0 +1,14 @@
+import platform
+
+print("System:", platform.system())
+print("Node Name:", platform.node())
+print("Release:", platform.release())
+print("Version:", platform.version())
+print("Machine:", platform.machine())
+print("Processor:", platform.processor())
+print("Architecture:", platform.architecture())
+print("Platform:", platform.platform())
+print("Python Version:", platform.python_version())
+print("Python Implementation:", platform.python_implementation())
+print("libc Version:", platform.libc_ver())
+print("Uname:", platform.uname())
