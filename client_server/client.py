97c702a03c49af054df08e3558648ed8ee491e00
samueltexa <woolardsamuels@gmail.com>
2024-11-27 12:54:26 +0300

    update clinet file

--- client_server/client.py
+++ client_server/client.py
@@ -5,12 +5,12 @@ def client_program():
     port = 50000
 
     client_socket = socket.socket()
-    client_socket.connect((host, port))  # Connect to the server
+    client_socket.connect((host, port))
 
-    device_name = client_socket.recv(1024).decode()  # Receive the device name from the server
+    device_name = client_socket.recv(1024).decode()
     print(f"Connected to server, the server's device name is: {device_name}")
 
-    client_socket.close()  # Close the client socket
+    client_socket.close()
 
 if __name__ == '__main__':
     client_program()
